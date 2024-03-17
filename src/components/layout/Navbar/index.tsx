b5c42731ceccdbe15861737cc12704a6999d4e2a
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-03-17 19:47:00 -0300

    styling navbar

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -3,10 +3,16 @@ import NavLink from "./NavLink";
 
 export default function Navbar() {
   return (
-    <div className="bg-violet-900 text-white p-5 flex shadow-lg">
-      {NAV_LINKS.map(({ href, label }) => (
-        <NavLink label={label} href={href} />
-      ))}
-    </div>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 p-5">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="w-full md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {NAV_LINKS.map(({ href, label }) => (
+              <NavLink href={href} label={label} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
